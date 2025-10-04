0be8e10f30b62c2aad75a6aed5b9ad98773544c0
Jack <aquatically123@gmail.com>
2025-10-04 15:52:31 +0100

    add instructions to join, add percentage indicator, change server data format to include capacity

--- app.py
+++ app.py
@@ -14,10 +14,17 @@ def data():
 
 @app.route("/serverlist")
 def server_list():
-    servers = [{"name": "Server 1", "online": "12", "ip": "127.0.0.1", "port": "1324", "password": "anotheruser"}, {"name": "Server 2", "online": "15", "ip": "127.0.0.1", "port": "1324", "password": "milk"}]
+    servers = [{"name": "Server 1", "online": "12", "capacity": "24", "ip": "127.0.0.1", "port": "1324", "password": "anotheruser"}, {"name": "Server 2", "online": "15", "capacity": "24", "ip": "127.0.0.1", "port": "1324", "password": "milk"}]
     rendered_servers = ""
     for server in servers:
-        rendered_servers = rendered_servers + f"{(render_template("serverlist.html", name=server["name"], online=server["online"], ip=server["ip"], port=server["port"], password=server["password"]))}"
+        percentage = (int(server["online"]) / int(server["capacity"])) * 100
+        if percentage <= 50:
+            colour = "green"
+        elif percentage <= 75:
+            colour = "amber"
+        else:
+            colour = "red"
+        rendered_servers = rendered_servers + f"{(render_template("serverlist.html", name=server["name"], online=server["online"], ip=server["ip"], port=server["port"], password=server["password"], percentage=percentage, colour=colour))}"
     return rendered_servers
 
 @app.route("/eventinfo")
@@ -32,6 +39,10 @@ def submit_level():
     else:
         open = False
     return render_template("submitlevel.html", form_link="https://docs.google.com/forms/d/e/1FAIpQLSdY048SsmOonBgR_u5R6QDFcViwNoxHHw9A2rOjmMCeTds4Rw/viewform", open=open)
+
+@app.route("/serverhelp")
+def server_help():
+    return render_template("serverhelp.html", discord_link="https://discord.gg/Ywa4jfn8hG")
 
 if __name__ == "__main__":
     app.run(debug=True)
