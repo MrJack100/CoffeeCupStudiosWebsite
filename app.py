fceb4b678d7bec77f914ce8a333c62f2c092a2ab
Jack <aquatically123@gmail.com>
2025-10-05 13:58:57 +0100

    Add mobile responsive navbar with desktop support, including 2 pages already

--- app.py
+++ app.py
@@ -3,9 +3,43 @@ import time
 
 app = Flask(__name__)
 
+menu_script = '''<script>
+        function openMenu() {
+            var x = document.getElementById("menu-mobile");
+            if (x.style.display === "flex") {
+                x.style.display = "none";
+            } else {
+                x.style.display = "flex";
+            }
+        } 
+    </script>'''
+
+menu_html = '''<section id="navbar-section">
+        <span onclick="openMenu()" class="material-symbols-outlined">
+            menu
+        </span>
+        <p id="header">Coffee Cup Studios</p>
+        <div id="menu-mobile">
+            <p class="menu-component" id="header">Coffee Cup Studios</p>
+            <a class="menu-component" href="#">Mod Installer</a>
+            <a class="menu-component" href="/submitlevel">Submit Level</a>
+        </div>
+    </section>
+    <section id="information-section">
+        <div>
+            <p>
+                Hello Pioneers, John Coffee Cup from Coffee Cup Studios here. I've just learned that Coffee Stain
+                Studios will be releasing their game on console this November. This will give them a huge leg up in the
+                market, so we will have to respond. I believe the best way to ensure a victory over the “Stainers” is to
+                build a giant obstacle-course tower and race factory carts to the top.</br></br>
+                -John Coffee Cup (Founder & CEO)</p>
+            <a href="/eventinfo">Event Information</a>
+        </div>
+    </section>'''
+
 @app.route("/")
 def root():
-    return render_template("index.html")
+    return render_template("index.html", menu_script=menu_script, menu_html=menu_html)
 
 @app.route("/data")
 def data():
@@ -29,7 +63,7 @@ def server_list():
 
 @app.route("/eventinfo")
 def event_info():
-    return render_template("eventinfo.html", discord_link="https://discord.gg/Ywa4jfn8hG")
+    return render_template("eventinfo.html", discord_link="https://discord.gg/Ywa4jfn8hG", menu_script=menu_script, menu_html=menu_html)
 
 @app.route("/submitlevel")
 def submit_level():
@@ -38,11 +72,11 @@ def submit_level():
         open = True
     else:
         open = False
-    return render_template("submitlevel.html", form_link="https://docs.google.com/forms/d/e/1FAIpQLSdY048SsmOonBgR_u5R6QDFcViwNoxHHw9A2rOjmMCeTds4Rw/viewform", open=open)
+    return render_template("submitlevel.html", form_link="https://docs.google.com/forms/d/e/1FAIpQLSdY048SsmOonBgR_u5R6QDFcViwNoxHHw9A2rOjmMCeTds4Rw/viewform", open=open, menu_script=menu_script, menu_html=menu_html)
 
 @app.route("/serverhelp")
 def server_help():
-    return render_template("serverhelp.html", discord_link="https://discord.gg/Ywa4jfn8hG")
+    return render_template("serverhelp.html", discord_link="https://discord.gg/Ywa4jfn8hG", menu_script=menu_script, menu_html=menu_html)
 
 if __name__ == "__main__":
     app.run(debug=True)
